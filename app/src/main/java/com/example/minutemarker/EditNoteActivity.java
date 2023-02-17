63d5f2315496fde3f148efb55fbc1911eba93a3e
MatthewMcNatt <matthew.mcnatt@mavs.uta.edu>
2023-02-17 11:35:31 -0600

    added a lot of comments, and going to merge into main

--- app/src/main/java/com/example/minutemarker/EditNoteActivity.java
+++ app/src/main/java/com/example/minutemarker/EditNoteActivity.java
@@ -14,6 +14,7 @@ import java.util.HashSet;
 
 public class EditNoteActivity extends AppCompatActivity {
 
+    //The note to work on
     int noteId;
 
     @Override
@@ -21,11 +22,16 @@ public class EditNoteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_note);
 
+        //text editor UI binding(boilerPlate)
         EditText editText = (EditText) findViewById(R.id.editText);
 
         Intent intent = getIntent();
-        noteId = intent.getIntExtra("noteId", -1);
 
+        //this attempts to pull the note selected into the screen context.
+        //The minus one is default value, in other words, if there is
+        //no not associated with the id, the use case is creating a new note
+        //and a new note is created.
+        noteId = intent.getIntExtra("noteId", -1);
         if(noteId != -1 ){
             editText.setText(MainActivity.notes.get(noteId));
         }else{
@@ -34,29 +40,36 @@ public class EditNoteActivity extends AppCompatActivity {
             MainActivity.arrayAdapter.notifyDataSetChanged();
         }
 
+        //Event Listeners
+        //Two are blank but left intentionally
         editText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
+                //Maybe needed later?
             }
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
+                //IMPORTANT: this is the heart of the note permanence PER APPLICATION INSTANCE.
+                //Every time text changes, the notes in main activity is updated
                 MainActivity.notes.set(noteId, String.valueOf(charSequence));
+                //this tells the link between UI displaying notes in MAIN MENU  that notes changed
                 MainActivity.arrayAdapter.notifyDataSetChanged();
 
+                //IMPORTANT: this is the heart of the note permanence ACROSS APPLICATION INSTANCE.
+                //this is basically a file link. This line opens connection
                 SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.minutemarker;", Context.MODE_PRIVATE);
-
+                //this line creates a hashset which shared preferneces is capable of storing
                 HashSet<String> set = new HashSet(MainActivity.notes);
-
+                //This is what writes to the file
                 sharedPreferences.edit().putStringSet("notes", set).apply();
 
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-
+                //Maybe needed later?
             }
         });
 
--- app/src/main/java/com/example/minutemarker/MainActivity.java
+++ app/src/main/java/com/example/minutemarker/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.minutemarker;
-
+/*
+Last Modified Matthew McNatt: 2/17
+*/
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,10 +24,13 @@ import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
 
+    //text array and the arraylist adapter
+    //The adapter pipes the strings into the listview format
     static ArrayList<String> notes = new ArrayList<>();
     static ArrayAdapter arrayAdapter;
 
     @Override
+    //This is component instantiates the menu
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.add_note_menu, menu);
@@ -34,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    //This is the event handler for the menu, my understanding is that new
+    //items can be added by adding a new entry into menu xml file( located res/menu)
+    // and adding the corresponding lines in nCreateOptionsMenu and here
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         super.onOptionsItemSelected(item);
         if(item.getItemId()==R.id.add_note){
@@ -46,44 +54,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    //hello the issue is fixed
-    //this is a basic branch test
+    //Code called at startup of app
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //The list of notes
         ListView listview = (ListView) findViewById(R.id.listView);
 
+        //IMPORTANT: this is the code that loads notes FROM shared preferences
+        //it fills set, and if set is not null, it adds a single example note.
         SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.minutemarker;", Context.MODE_PRIVATE);
-
         HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("notes", null);
-
         if(set == null){
             notes.add("example note");
         }else{
             notes= new ArrayList(set);
         }
 
+
+        //This creates and links the adapter to the notes string data
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes );
 
+        //hooks the adapter up to listview
         listview.setAdapter(arrayAdapter);
 
+
+        //Navigation: on note being clicked. Sends noteId to note edit screen
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
+                //screen to go to
                 Intent intent = new Intent(getApplicationContext(), EditNoteActivity.class);
+                //send noteId
                 intent.putExtra("noteId", i);
                 startActivity(intent);
             }
         });
 
+        //This is an event handler, on holding down click on a message, it pops a dialogue to delete it
         listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
 
+                //needed or app will crash
                 int itemToDelete = i;
 
+                //evil android.dialoguebuilder syntax
                 new AlertDialog.Builder(MainActivity.this)
                         .setIcon(android.R.drawable.ic_dialog_alert)
                         .setTitle("Are you sure?")
@@ -91,14 +108,15 @@ public class MainActivity extends AppCompatActivity {
                         .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
+                                //THIS IS THE LOGIC THAT DELETES FROM NOTES LOCAL INSTANCE
                                 notes.remove(itemToDelete);
                                 arrayAdapter.notifyDataSetChanged();
 
+                                //THIS IS THE LOGIC THAT SAVES IT. see on text change in EditNoteActivity for explanation
                                 SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.minutemarker;", Context.MODE_PRIVATE);
-
                                 HashSet<String> set = new HashSet(MainActivity.notes);
-
                                 sharedPreferences.edit().putStringSet("notes", set).apply();
+                                //end of deleting logic
                             }
                         })
                         .setNegativeButton("No", null)

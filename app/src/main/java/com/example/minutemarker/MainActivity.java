4282e3c2c919f0f6f68715756ce62d1200989b0b
MatthewMcNatt <matthew.mcnatt@mavs.uta.edu>
2023-02-17 09:32:06 -0600

    testing some basic branch functionality

--- app/src/main/java/com/example/minutemarker/MainActivity.java
+++ app/src/main/java/com/example/minutemarker/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     //hello the issue is fixed
+    //this is a basic branch test
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);

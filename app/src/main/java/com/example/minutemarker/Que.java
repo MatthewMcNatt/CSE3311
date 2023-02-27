4c7c0ed78c1b49dafee977fff03fd4bb59c0623e
MatthewMcNatt <matthew.mcnatt@mavs.uta.edu>
2023-02-27 08:05:40 -0600

    que type is implemented, moving on to detector

---
+++ app/src/main/java/com/example/minutemarker/Que.java
@@ -0,0 +1,48 @@
+package com.example.minutemarker;
+/*
+Last Modified Matthew McNatt: 2/17
+*/
+
+import java.util.ArrayList;
+
+/*
+* Que is an object to hold all the trigger words and
+* phrases for an action as well as possibly a lambda expression
+* that would represent the action. The latter will depend
+* highly on implementation.
+*/
+public class Que {
+    //used as identifier
+    private String _title;
+    private ArrayList<String> _triggers;
+
+    //getters & setters
+    public String getTitle() {
+        return _title;
+    }
+
+    public void setTitle(String title) {
+        this._title = title;
+    }
+
+    //constructors
+
+    //with array list
+    public Que(String title, ArrayList<String> triggers){
+        if(triggers == null){
+            triggers = new ArrayList<String>();
+        } else {_triggers = triggers;}
+
+        _title = title;
+    }
+    //without arraylist
+    public Que(String title){
+        _triggers = new ArrayList<String>();
+        _title = title;
+    }
+
+
+
+
+
+}
--- app/src/test/java/com/example/minutemarker/QueCheckTest.java
+++ app/src/test/java/com/example/minutemarker/QueCheckTest.java
@@ -5,11 +5,12 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * Matthew McNatt: 2/27/2023
+ * The Testing cases for the que word checker
+ * The que word checker is merely the detection,
+ * not the associated response
  */
-public class ExampleUnitTest {
+public class QueCheckTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
